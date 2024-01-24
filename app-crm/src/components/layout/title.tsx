64c2cd8e58b2683765e63f32913b147123b44357
ioannisniozas <giannis.niozas@gmail.com>
2024-01-24 18:08:23 +0200

    Name change

--- app-crm/src/components/layout/title.tsx
+++ app-crm/src/components/layout/title.tsx
@@ -9,7 +9,7 @@ import { Logo } from "./logo";
 
 const { useToken } = theme;
 
-const name = "News Tracker";
+const name = "NewsWatch";
 
 export const Title: React.FC<RefineLayoutThemedTitleProps> = ({
     collapsed,

c2c422723d76c2bc343a640c0e602f93ae5119d3
ioannisniozas <giannis.niozas@gmail.com>
2024-01-31 22:35:59 +0200

    Front and back end changes

--- verse/src/test/java/com/getout/verse/ScheduledTasksTest.java
+++ verse/src/test/java/com/getout/verse/ScheduledTasksTest.java
@@ -1,28 +1,28 @@
-package com.getout.verse;
-
-import com.getout.component.ScheduledTasks;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Arrays;
-import java.util.List;
-
-@SpringBootTest
-public class ScheduledTasksTest {
-
-    @Autowired
-    private ScheduledTasks scheduledTasks;
-
-    @Test
-    public void testScheduleKeywordCountTask() {
-        List<String> keywords = Arrays.asList("israeli", "israel", "palestine", "palestinian", "palestinians", "hamas", "israelis");
-        String fromIndex = "cnn_articles_newone";
-        String toIndex = "cnn_articles_newone_counts3";
-        String startDate = "2023-10-01";
-        String endDate = "2023-12-31";
-        scheduledTasks.scheduleKeywordCountTask(keywords, fromIndex, toIndex, startDate, endDate);
-    }
-
-
-}
+//package com.getout.verse;
+//
+//import com.getout.component.ScheduledTasks;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//@SpringBootTest
+//public class ScheduledTasksTest {
+//
+//    @Autowired
+//    private ScheduledTasks scheduledTasks;
+//
+//    @Test
+//    public void testScheduleKeywordCountTask() {
+//        List<String> keywords = Arrays.asList("israeli", "israel", "palestine", "palestinian", "palestinians", "hamas", "israelis");
+//        String fromIndex = "cnn_articles_newone";
+//        String toIndex = "cnn_articles_newone_counts3";
+//        String startDate = "2023-10-01";
+//        String endDate = "2023-12-31";
+//        scheduledTasks.scheduleKeywordCountTask(keywords, fromIndex, toIndex, startDate, endDate);
+//    }
+//
+//
+//}

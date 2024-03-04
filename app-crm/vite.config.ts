3939b9f53aa1c146d4817f2430d35ccef761295a
ioannisniozas <giannis.niozas@gmail.com>
2024-03-04 23:19:52 +0200

    Added keyword changes

--- app-crm/vite.config.ts
+++ app-crm/vite.config.ts
@@ -5,6 +5,16 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [tsconfigPaths(), react()],
+    server: {
+        // Conditionally configure the server based on the environment
+          cors: {
+            // Development-specific CORS configuration
+            origin: 'http://localhost:5173',
+            credentials: true,
+          }
+        },
+        // Additional development server configurations can go here
+   
     build: {
         rollupOptions: {
             output: {
